26b47c5d3bbb5c805ee992295ac9541b7760bb79
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-04-21 21:54:40 -0300

    Add components header and player

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/global.scss'
+import { Header } from '../components/Header'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Header />
+       <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
