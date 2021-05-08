86e510dd1d0eda81909f4548b7b05ad77289e199
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-05-08 12:50:46 -0300

    Created Player layout and api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/global.scss'
 import { Header } from '../components/Header'
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-       <Component {...pageProps} />
+    <div className={styles.wrapper}>
+      <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
     </div>
   )
 }
